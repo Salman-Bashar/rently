9b7f142d084ecddf0ae9fca3e059540944047d59
Salman Bashar <salman35-1493@diu.edu.bd>
2020-06-08 20:38:02 +0600

    Add files via upload
    - Added database 
    - Made the code more structured

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 
-const Joi = require('joi');
+const mongoose = require('mongoose');
 const home = require('./routes/home');
 const genres = require('./routes/genres');
+const customers = require('./routes/customers');
 const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
 
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect('mongodb://localhost/rently')
+    .then(() => console.log('Connected to Mongodb...'))
+    .catch(err => console.error('Error', err.message));
+
 app.use(express.json());
 app.use('/', home);
 app.use('/api/genres', genres);
+app.use('/api/customers', customers);
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ routes/customers.js
@@ -0,0 +1,65 @@
+
+const { Customer, validateCustomer } = require('../models/customer');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+
+//Get all customers
+router.get('/', async (req, res) => {
+    const customers = await Customer.find().sort('name');
+    res.send(customers);
+});
+
+//Get a specific customer
+router.get('/:id', async (req, res) => {
+    const customer = await Customer.findById(req.params.id); 
+ 
+    if (!customer) return res.status(404).send('No customer found for this ID. Sorry!');
+ 
+    res.send(customer);
+});
+
+//Create a new customer
+router.post('/', async (req, res) => {
+    const { error } = validateCustomer(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let customer = new Customer ({ 
+        name: req.body.name,
+        phone: req.body.phone, 
+        isGold: req.body.isGold 
+    });
+    customer = await customer.save();
+    
+    res.send(customer);
+});
+
+//Update a customer
+router.put('/:id', async (req, res) => {
+    const { error } = validateCustomer(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const customer = await Customer
+        .findByIdAndUpdate(req.params.id, 
+            { 
+                name : req.body.name,  
+                phone: req.body.phone, 
+                isGold: req.body.isGold
+            }, { new: true });
+  
+    if (!customer) return res.status(404).send('No customer found for this ID. Sorry!');
+
+    res.send(customer);
+});
+
+//Delete a customer
+router.delete('/:id', async (req, res) => {
+    const customer = await Customer.findByIdAndRemove(req.params.id);
+  
+    if (!customer) return res.status(404).send('No customer found for this ID. Sorry!');
+
+    res.send(customer);
+});
+
+module.exports = router;
